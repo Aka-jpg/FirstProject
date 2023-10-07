ee524037d4eecd99fd133bc350ab182f4d9c21fa
Akash Nepal <akashnepal72@gmail.com>
2023-10-07 11:34:48 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+console.log("script running")
+document.querySelector('.cross').style.display='none';
+document.querySelector('.menu').addEventListener("click",()=>{
+    document.querySelector('.sidebar').classList.toggle('sidebargo');
+if(document.querySelector('.sidebar').classList.contains('sidebargo')){
+    document.querySelector('.menu').style.display='inline'
+    document.querySelector('.cross').style.display='none'
+}
+else{
+    document.querySelector('.menu').style.display='none';
+    setTimeout(()=>{
+    document.querySelector('.cross').style.display='inline';
+},400);
+}
+})
+document.querySelector('.cross').addEventListener("click",()=>{
+    document.querySelector('.menu').style.display='inline';
+    document.querySelector('.sidebar').classList.toggle('sidebargo');
+    document.querySelector('.cross').style.display='none';
+})
